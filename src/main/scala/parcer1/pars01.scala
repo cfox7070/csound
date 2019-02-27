8a5933a5c3639877b7b27a18e9f69b7519e9aadd
cfox70 <cfox7070@gmail.com>
2019-02-28 00:15:38 +0300

    notes changed

--- src/main/scala/parcer1/pars01.scala
+++ src/main/scala/parcer1/pars01.scala
@@ -72,22 +72,24 @@ object Measure {
         val rcom(vl,co)=str
         val rval="""m([0-9]+)\s+([0-9]+)""".r
         val rval(bits,num)=vl
-        Note.curStart=bits.toInt*(num.toInt-1)
+        Note.curMStart=bits.toInt*(num.toInt-1)
+        Note.curStart=0
         new Measure(bits.toInt,num.toInt,co)
    }
    def apply(bits:Int,num:Int,cmts:String):Measure ={        
-        Note.curStart=bits*(num-1)
+        Note.curMStart=bits*(num-1)
+        Note.curStart=0
         new Measure(bits,num,cmts)
    }
 }
 
-class Note (ninst:Int,start:Float,dur:Float,pitch:Int,okt:Int,cmts:String) extends ScoreElem(){
+class Note (st:String, ninst:Int,start:Float,dur:Float,pitch:String,cmts:String) extends ScoreElem(){
 /*
 ([rcdefgah]([0-9]*)(#[+-]?|##[+-]?|b[+-]?|bb[+-]?)*)([\t ]+([a-z]+)([0-9]*))*([\t ]*\/\/.*)*$
 */
     override def outStr():String= {
-		val cmt= if((cmts!=null) && (cmts!="")) s"   //$cmts" else ""
-		s"i $ninst $start $dur $okt.$pitch  $cmt"
+		val cmt= if((cmts!=null) && (cmts!="")) s"\t;$st\t$cmts" else s"\t;$st"
+		s"i $ninst \t%.4f\t%.4f\t 0.5 \t $pitch \t  $cmt".formatLocal(java.util.Locale.US,start,dur)
 	}
 
 }
@@ -95,26 +97,64 @@ class Note (ninst:Int,start:Float,dur:Float,pitch:Int,okt:Int,cmts:String) exten
 object Note {
    var curNinst:Int=1
    var curStart:Float=0f
+   var curMStart:Float=0f
    var curDur:Float=0f
-   var curPitch:Int=0
-   var curOkt:Int=0
+   var curOkt:Int=8
+   
+    val lpch=Map("c" ->"00",
+				 "c#"->"01",
+				 "db"->"01",
+				 "d" ->"02",
+				 "d#"->"03",
+				 "eb"->"03",
+				 "e" ->"04",
+				 "e#"->"05",
+				 "fb"->"04",
+				 "f" ->"05",
+				 "f#"->"06",
+				 "gb"->"06",
+				 "g" ->"07",
+				 "g#"->"08",
+				 "ab"->"08",
+				 "a" ->"09",
+				 "a#"->"10",
+				 "bb"->"10",
+				 "b" ->"11",
+				 "b#"->"00",
+				 "r" ->"")
+	val durVal = Map( "1" ->4.0f, 
+					  "2" ->2f,
+					  "4" ->1f,
+					  "8" ->0.5f,
+					  "16"->0.25f,
+					  "32"->0.125f,
+					  "64"->(1.0/16.0).toFloat)
 
-   def apply(str : String):Note ={
+   def apply(str : String, sco:ArrayBuffer[ScoreElem]):Unit ={
 	val rcom="""^([^\/]*)(?:\/\/(.*))?$""".r //value and comments
-	val pitchex="^[cdefgahr].".r
+	val pitchex="^([cdefgar#b]+)([0-9]*)".r
 	val inum="^i([0-9]+)".r
+	val dur="^dur([0-9]+)".r
+	val st="^st([0-9]+)".r
 	
   	var rcom(vl,co)=str
 	if(co!=null && co!="") co="//"+co
     val vls=vl.split("[ \t]+")
-	var nts=new ArrayBuffer[String]()
+	var nts=new ArrayBuffer[(Int,String,String)]()
 	for (s <- vls){
 		s match {
-			case pitchex(_*) => nts+=s
+			case pitchex(p,o) => { if (o!="")curOkt=o.toInt; nts+= ((curOkt,lpch(p),s))}
 			case inum(in) => curNinst=in.toInt
+			case dur(nm) => curDur=durVal(nm)
+			case st(nm) => curStart=nm.toFloat
+			case _ =>
 		}
 	}
-    new Note(curNinst,curStart,curDur,curPitch,curOkt,co)
+	//println(nts)
+	for ((o,p,st) <- nts){
+		if(p!="") sco+=new Note(st,curNinst,curMStart+curStart,curDur,s"$o.$p",co)
+	}
+	curStart+=curDur
    }
 }
 
@@ -150,7 +190,7 @@ object score extends App with StrFns{
     }
    
    def readFile(iter:Iterator[String]){
-       val note="^[cdefgahr].*".r
+       val note="^[cdefgabr].*".r
  //     val measure="^m.*".r
       val measure="""^[m]([0-9]+)[\t ]+([0-9])+(?:[\t ]*\/\/(.*))?$""".r
       val comments="^//(.*)".r
@@ -159,7 +199,7 @@ object score extends App with StrFns{
         val tstr=iter.next.trim
         tstr match {
             case ""=>
-            case note(_*) => println("note");mSco +=Note(tstr)      // delete
+            case note(_*) => println("note");Note(tstr,mSco)      // delete
             case measure(bits,num,cmt) =>{println(s"$bits $num $cmt");mSco += Measure(bits.toInt,num.toInt,strOrCms(cmt)) }    
             case comments(cmt) => {println(s";$cmt");mSco+=Comments(strOrCms(cmt)) }      // delete
             case native(_*) => {println(";native");mSco+=Native(tstr,iter) }       // delete.length>0
